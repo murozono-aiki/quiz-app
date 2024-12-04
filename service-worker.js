425a66ab793bbe36b511710dbccc7a3f95e35799
murozono-aiki <murozono.aiki@gmail.com>
2024-12-04 21:50:12 +0900

    サービスウォーカー

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+var cacheName = "QuizAppPWA-v1";
+var appShellFiles = [
+  "./index.html",
+  "./script.js",
+  "./favicon.ico",
+  "./icons/quiz (192).png",
+  "./icons/quiz (512).png",
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(appShellFiles);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
