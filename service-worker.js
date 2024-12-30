82f18efd8ae4400014c5a6a126664fbaa38bfc6d
murozono-aiki <151797695+murozono-aiki@users.noreply.github.com>
2024-12-30 22:01:11 +0900

    サービスワーカーを更新

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = "QuizAppPWA-v10.8";
+var cacheName = "QuizAppPWA";
 var appShellFiles = [
   "./index.html",
   "./script.js",
@@ -9,22 +9,59 @@ var appShellFiles = [
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(appShellFiles);
-    })
+      caches.open(cacheName).then(cache => {
+          return cache.addAll(appShellFiles);
+      })
   );
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== cacheName) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-});
+
+/**
+* @param {Request} request
+*/
+async function cacheResponse(request) {
+  const cachedResponse = await caches.match(request);
+  if (cachedResponse) {
+      return cachedResponse;
+  }
+  try {
+      const networkResponse = await fetch(request);
+      if (networkResponse.ok) {
+          const cache = await caches.open(cacheName);
+          cache.put(request, networkResponse.clone());
+      }
+      return networkResponse;
+  } catch (error) {
+      return Response.error();
+  }
+}
+
+/**
+* @param {Request} request
+*/
+async function networkResponse(request) {
+  const networkResponse = await fetch(request);
+  if (networkResponse.ok) {
+      const cache = await caches.open(cacheName);
+      cache.put(request, networkResponse.clone());
+  }
+  return networkResponse;
+}
+
+/**
+* @param {Request} request
+*/
+async function cache(request) {
+  const cacheResponsePromise = cacheResponse(request);
+  const networkResponsePromise = networkResponse(request);
+  return (await cacheResponsePromise) || (await networkResponsePromise);
+}
+
+/**
+* @param {Event} event
+*/
+function onFetch(event) {
+  event.respondWith(cache(event.request));
+}
+
+self.addEventListener("fetch", onFetch);
