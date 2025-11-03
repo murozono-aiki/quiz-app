cff0ddea121a51cf7c3350c20cdc14a3cf0fbb02
murozono-aiki <151797695+murozono-aiki@users.noreply.github.com>
2025-11-03 11:04:29 +0900

    サービスワーカーの改良

--- service-worker.js
+++ service-worker.js
@@ -9,9 +9,9 @@ var appShellFiles = [
 
 self.addEventListener("install", event => {
   event.waitUntil(
-      caches.open(cacheName).then(cache => {
-          return cache.addAll(appShellFiles);
-      })
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(appShellFiles);
+    })
   );
 });
 
@@ -22,17 +22,17 @@ self.addEventListener("install", event => {
 async function cacheResponse(request) {
   const cachedResponse = await caches.match(request);
   if (cachedResponse) {
-      return cachedResponse;
+    return cachedResponse;
   }
   try {
-      const networkResponse = await fetch(request);
-      if (networkResponse.ok) {
-          const cache = await caches.open(cacheName);
-          cache.put(request, networkResponse.clone());
-      }
-      return networkResponse;
+    const networkResponse = await fetch(request);
+    if (networkResponse.ok) {
+      const cache = await caches.open(cacheName);
+      cache.put(request, networkResponse.clone());
+    }
+    return networkResponse;
   } catch (error) {
-      return Response.error();
+    return Response.error();
   }
 }
 
@@ -42,8 +42,8 @@ async function cacheResponse(request) {
 async function networkResponse(request) {
   const networkResponse = await fetch(request);
   if (networkResponse.ok) {
-      const cache = await caches.open(cacheName);
-      cache.put(request, networkResponse.clone());
+    const cache = await caches.open(cacheName);
+    cache.put(request, networkResponse.clone());
   }
   return networkResponse;
 }
@@ -52,13 +52,14 @@ async function networkResponse(request) {
 * @param {Request} request
 */
 async function cache(request) {
-  if (request.method == "GET") {
+  const requestUrl = new URL(request.url);
+  if (request.method == "GET" && requestUrl.origin == location.origin) {
     const cacheResponsePromise = cacheResponse(request);
     const networkResponsePromise = networkResponse(request);
     return (await cacheResponsePromise) || (await networkResponsePromise);
-} else {
+  } else {
     return await fetch(request);
-}
+  }
 }
 
 /**
